35a0db8070fb6db09175edb482c9479c363ab26f
Sam Stow <sam@appfactory.com.au>
2014-08-08 23:18:12 +1000

    added skeletons for loan and testLoan

---
+++ src/library/tests/entities/TestLoan.java
@@ -0,0 +1,70 @@
+package library.tests.entities;
+
+import static org.junit.Assert.*;
+import static org.easymock.EasyMock.*;
+
+import java.util.Date;
+
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.IMember;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLoan {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testValidConstructor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInvalidConstructor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCommit() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testComplete() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIsOverDue() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCheckOverDue() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetBorrower() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetBook() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+}
