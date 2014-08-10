feb1c5785f70aa7e40c4c6150e35622a591d59d0
Sam Stow <sam@appfactory.com.au>
2014-08-10 12:03:46 +0800

    Implemented bookHelper method

--- src/library/classes/daos/BookHelper.java
+++ src/library/classes/daos/BookHelper.java
@@ -1,14 +1,14 @@
 package library.classes.daos;
 
+import library.classes.entities.Book;
 import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
 
 public class BookHelper implements IBookHelper {
 
 	@Override
-	public IBook makeBook(String author, String title, String callNumber, int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public IBook makeBook(String author, String title, String callNumber, int id) throws IllegalArgumentException {
+		return new Book(author, title, callNumber, id);
 	}
 
 }
