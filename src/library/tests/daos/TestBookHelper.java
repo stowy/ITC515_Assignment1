e57e8210bfd0d21a7a65845a65f63e5ef1201f26
Sam Stow <sam@appfactory.com.au>
2014-08-10 12:02:29 +0800

    implemented test for bookHelper

--- src/library/tests/daos/TestBookHelper.java
+++ src/library/tests/daos/TestBookHelper.java
@@ -1,6 +1,8 @@
 package library.tests.daos;
 
 import static org.junit.Assert.*;
+import library.classes.daos.BookHelper;
+import library.interfaces.entities.IBook;
 
 import org.junit.After;
 import org.junit.Before;
@@ -8,6 +10,11 @@ import org.junit.Test;
 
 public class TestBookHelper {
 
+	private String author = "author";
+	private String title = "title";
+	private String callNumber = "callNumber";
+	private int bookId = 1;
+	
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -18,7 +25,10 @@ public class TestBookHelper {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		BookHelper helper = new BookHelper();
+		IBook book = helper.makeBook(author, title, callNumber, bookId);
+		assertNotNull(book);
+		assertTrue(book instanceof IBook);
 	}
 
 }
