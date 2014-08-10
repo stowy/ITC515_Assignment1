3ff37dcf438b5f2ab60f84e9e483fbaaafec7479
Sam Stow <sam@appfactory.com.au>
2014-08-10 13:44:46 +0800

    Added skeletons for testing BookDAO

---
+++ src/library/classes/daos/BookDAO.java
@@ -0,0 +1,46 @@
+package library.classes.daos;
+
+import java.util.List;
+
+import library.interfaces.daos.IBookDAO;
+import library.interfaces.entities.IBook;
+
+public class BookDAO implements IBookDAO {
+
+	@Override
+	public IBook addBook(String author, String title, String callNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IBook getBookByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IBook> listBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IBook> findBooksByAuthor(String author) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IBook> findBooksByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IBook> findBooksByAuthorTitle(String author, String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/library/tests/daos/TestBookDAO.java
@@ -0,0 +1,63 @@
+package library.tests.daos;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import library.interfaces.entities.IBook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBookDAO {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testValidConstructor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInvalidConstructor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBookByID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListBooks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindBooksByAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindBooksByTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindBooksByAuthorTitle() {
+		fail("Not yet implemented");
+	}
+
+}
