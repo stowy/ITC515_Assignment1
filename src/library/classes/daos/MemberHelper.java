940a2b7540e8bee4762adf791f71258066cb0c92
Sam Stow <sam@appfactory.com.au>
2014-08-10 12:12:15 +0800

    Added skeletons for MemberHelper

---
+++ src/library/classes/daos/MemberHelper.java
@@ -0,0 +1,15 @@
+package library.classes.daos;
+
+import library.interfaces.daos.IMemberHelper;
+import library.interfaces.entities.IMember;
+
+public class MemberHelper implements IMemberHelper {
+
+	@Override
+	public IMember makeMember(String firstName, String lastName,
+			String contactPhone, String emailAddress, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
