fb5e24bced8553bf96e81a63aef98142f92e6fd1
Sam Stow <sam@appfactory.com.au>
2014-08-10 11:57:48 +0800

    Added skeleton for BookHelper

---
+++ src/library/classes/daos/BookHelper.java
@@ -0,0 +1,14 @@
+package library.classes.daos;
+
+import library.interfaces.daos.IBookHelper;
+import library.interfaces.entities.IBook;
+
+public class BookHelper implements IBookHelper {
+
+	@Override
+	public IBook makeBook(String author, String title, String callNumber, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
