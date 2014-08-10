70195fa441467412e0ad3a4c7a017daddabb1195
Sam Stow <sam@appfactory.com.au>
2014-08-11 08:14:23 +1000

    Added skeletons for Testing and Implementing MemberDAO

---
+++ src/library/tests/daos/TestMemberDAO.java
@@ -0,0 +1,63 @@
+package library.tests.daos;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import library.interfaces.entities.IMember;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMemberDAO {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testValidConstructor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInvalidConstructor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddMember() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetMemberByID() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testListMembers() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFindMembersByLastName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindMembersByEmailAddress() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFindMembersByNames() {
+		fail("Not yet implemented");
+	}
+
+}
