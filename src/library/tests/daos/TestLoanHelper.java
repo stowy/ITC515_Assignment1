26bb3246dfad2293e0c65910da0ba022f664cdf5
Sam Stow <sam@appfactory.com.au>
2014-08-10 12:08:53 +0800

    Implemented test for loan helper

--- src/library/tests/daos/TestLoanHelper.java
+++ src/library/tests/daos/TestLoanHelper.java
@@ -1,24 +1,48 @@
 package library.tests.daos;
 
+import static org.easymock.EasyMock.createMock;
 import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import library.classes.daos.LoanHelper;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestLoanHelper {
+	
+	IBook book = createMock(IBook.class);
+	IMember member = createMock(IMember.class);
+	int loanId = 1;
+	Date borrowDate;
+	Date dueDate;
 
 	@Before
 	public void setUp() throws Exception {
+		Calendar calendar = Calendar.getInstance();
+		borrowDate = calendar.getTime();
+		calendar.add(Calendar.DAY_OF_YEAR, ILoan.LOAN_PERIOD);
+		dueDate = calendar.getTime();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		borrowDate = null;
+		dueDate = null;
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		LoanHelper helper = new LoanHelper();
+		ILoan loan = helper.makeLoan(book, member, borrowDate, dueDate, loanId);
+		assertNotNull(loan);
+		assertTrue(loan instanceof ILoan);
 	}
 
 }
