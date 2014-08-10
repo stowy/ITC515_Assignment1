92339be5a9036b1604f5de9abeba217cb4cfde34
Sam Stow <sam@appfactory.com.au>
2014-08-10 12:16:34 +0800

    Implemented test for member helper

--- src/library/tests/daos/TestMemberHelper.java
+++ src/library/tests/daos/TestMemberHelper.java
@@ -1,6 +1,8 @@
 package library.tests.daos;
 
 import static org.junit.Assert.*;
+import library.classes.daos.MemberHelper;
+import library.interfaces.entities.IMember;
 
 import org.junit.After;
 import org.junit.Before;
@@ -8,6 +10,12 @@ import org.junit.Test;
 
 public class TestMemberHelper {
 
+	String first = "first";
+	String last = "last";
+	String phone = "phone";
+	String email = "email";
+	int id = 1;
+	
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -18,7 +26,10 @@ public class TestMemberHelper {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		MemberHelper helper = new MemberHelper();
+		IMember member = helper.makeMember(first, last, phone, email, id);
+		assertNotNull(member);
+		assertTrue(member instanceof IMember);
 	}
 
 }
