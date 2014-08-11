1050412b878b4f1b8cf138994c740984d249be70
Sam Stow <sam@appfactory.com.au>
2014-08-11 18:37:18 +1000

    Implemented add member to member dao

--- src/library/classes/daos/MemberDAO.java
+++ src/library/classes/daos/MemberDAO.java
@@ -1,8 +1,9 @@
 package library.classes.daos;
 
+import java.util.ArrayList;
 import java.util.List;
-import static library.classes.utils.VerificationUtil.*;
 
+import static library.classes.utils.VerificationUtil.*;
 import library.interfaces.daos.IMemberDAO;
 import library.interfaces.daos.IMemberHelper;
 import library.interfaces.entities.IMember;
@@ -10,17 +11,23 @@ import library.interfaces.entities.IMember;
 public class MemberDAO implements IMemberDAO {
 
 	IMemberHelper memberHelper;
+	List<IMember> members;
+	int nextId;
 	
 	public MemberDAO(IMemberHelper memberHelper) throws IllegalArgumentException {
 		assertNotNull(memberHelper);
 		this.memberHelper = memberHelper;
+		this.members = new ArrayList<IMember>();
+		nextId = 1;
 	}
 	
 	@Override
 	public IMember addMember(String firstName, String lastName,
 			String ContactPhone, String emailAddress) {
-		// TODO Auto-generated method stub
-		return null;
+		IMember member = memberHelper.makeMember(firstName, lastName, ContactPhone, emailAddress, nextId);
+		members.add(member);
+		nextId++;
+		return member;
 	}
 
 	@Override
