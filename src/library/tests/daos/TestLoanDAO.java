93280fdbf05b53aa0ac380084f123bae2cb0988d
Sam Stow <sam@appfactory.com.au>
2014-08-11 19:12:29 +1000

    Implemented tests for create and get new pending list for LoanDAO

--- src/library/tests/daos/TestLoanDAO.java
+++ src/library/tests/daos/TestLoanDAO.java
@@ -3,12 +3,14 @@ package library.tests.daos;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import library.classes.daos.LoanDAO;
 import library.interfaces.daos.ILoanDAO;
 import library.interfaces.daos.ILoanHelper;
+import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 
@@ -18,12 +20,37 @@ import org.junit.Test;
 
 public class TestLoanDAO {
 
+	private ILoanHelper mockLoanHelper;
+	private ILoanDAO loanDAO;
+	
+	IBook mockBook;
+	IMember mockMember;
+	Date borrowDate;
+	Date dueDate;
+	
 	@Before
 	public void setUp() throws Exception {
+		mockLoanHelper = createMock(ILoanHelper.class);
+		loanDAO = new LoanDAO(mockLoanHelper);
+		
+		Calendar calendar = Calendar.getInstance();
+		borrowDate = calendar.getTime();
+		calendar.add(Calendar.DAY_OF_YEAR, ILoan.LOAN_PERIOD);
+		dueDate = calendar.getTime();
+		
+		mockBook = createMock(IBook.class);
+		mockMember = createMock(IMember.class);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		mockLoanHelper = null;
+		loanDAO = null;
+		
+		borrowDate = null;
+		dueDate = null;
+		mockBook = null;
+		mockMember = null;
 	}
 
 	@Test
@@ -41,7 +68,10 @@ public class TestLoanDAO {
 	
 	@Test
 	public void testCreateNewPendingList() {
-		fail("Not yet implemented");
+		loanDAO.createNewPendingList(mockMember);
+		List<ILoan> loans = loanDAO.getPendingList(mockMember);
+		assertNotNull(loans);
+		assertTrue(loans.size() == 0);
 	}
 
 	@Test
@@ -51,7 +81,15 @@ public class TestLoanDAO {
 	
 	@Test
 	public void testGetPendingList() {
-		fail("Not yet implemented");
+		loanDAO.createNewPendingList(mockMember);
+		List<ILoan> loans = loanDAO.getPendingList(mockMember);
+		assertNotNull(loans);
+		assertTrue(loans.size() == 0);
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testGetPendingListException() {
+		loanDAO.getPendingList(mockMember);
 	}
 	
 	@Test
